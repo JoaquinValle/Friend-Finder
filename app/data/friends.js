969d8acf404dc0c996542cbac4826c0f9c33d8fc
Joaquin Valle <jv346@cornell.edu>
2019-11-29 16:54:15 -0600

    created initial code structure for friends.js and changed survey answers

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,25 @@
 // 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
 
-// ```json
-// {
-//   "name":"Ahmed",
-//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//   "scores":[
-//       5,
-//       1,
-//       4,
-//       4,
-//       5,
-//       1,
-//       2,
-//       5,
-//       4,
-//       1
-//     ]
-// }
-// ```
+let object = {
+    name: "name",
+    photo: "link",
+    scores: [4, 3, 2]
+}
+
+let sampleScores = [1, 5, 1]
+
+console.log(object.scores)
+console.log(sampleScores)
+
+let matchScore = 0
+
+// otro for para comparar con todos (sumar todos y comparar...)
+
+for (let i = 0; i < object.scores.length; i++) {
+    matchScore += Math.abs(object.scores[i] - sampleScores[i]) 
+}
+
+console.log(matchScore)
 
 // 6. Determine the user's most compatible friend using the following as a guide:
 
