3acb2fec606249110dba6acfba75280fee8e50d2
Joaquin Valle <jv346@cornell.edu>
2019-12-02 13:03:43 -0600

    added html paths to display pages

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,3 +4,13 @@
 //    * A default, catch-all route that leads to `home.html` which displays the home page.
 
 const path = require("path")
+
+module.exports = (app) => {
+    app.get("/survey", (req, res) => {
+        res.sendfile(__dirname, "../public/survey.html")
+    })
+
+    app.get("*", (req, res) => {
+        res.sendfile(__dirname, "../public/home.html")
+    })
+}
