7eaff02f72cfb313951a76ab8492dc727651181d
Joaquin Valle <jv346@cornell.edu>
2019-12-02 15:40:11 -0600

    changed the logic from friends.js (use only for initial data) to apiRoutes.js and expanded question list from 3 to 10 in survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,32 @@
 let dataArr = [
   {
     name: "John",
-    photo: "link",
-    scores: [4, 3, 2],
+    photo: "www.myphotolink.com",
+    scores: [5, 4, 2, 2, 1, 3, 5, 2, 5, 5],
     age: 18,
     fact: "I enjoy walks on the beach"
   },
   {
     name: "James",
-    photo: "link",
-    scores: [1, 5, 2],
+    photo: "www.thispic.com",
+    scores: [2, 1, 4, 2, 3, 1, 1, 3, 5, 5],
     age: 25,
     fact: "I don't like to eat chocolate"
+  },
+  {
+    name: "Jack",
+    photo: "www.gifnpics.com",
+    scores: [1, 5, 4, 4, 3, 4, 5, 5, 5, 3],
+    age: 31,
+    fact: "I love water"
+  },
+  {
+    name: "Jill",
+    photo: "www.easyphoto2963.com",
+    scores: [2, 3, 3, 3, 3, 5, 1, 4, 5, 3],
+    age: 28,
+    fact: "I play squash"
   }
 ]
-
-let sampleScores = [1, 5, 1]
-
-let matchScoreArr = []
-
-dataArr.forEach(person => {
-    let matchScore = 0
-    for (let i = 0; i < person.scores.length; i++) {
-        matchScore += Math.abs(person.scores[i] - sampleScores[i]) 
-    }
-    matchScoreArr.push(matchScore)
-    console.log(`Match Score for ${person.name} is ${matchScore}`)
-})
-
-for (let i in matchScoreArr) {
-    dataArr[i].match = matchScoreArr[i]
-}
-console.log(dataArr)
 
 module.exports= dataArr
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,22 @@ module.exports = (app) => {
     })
 
     app.post("/api/friends", (req, res) => {
+        console.log(req.body)
+        let currentPerson = req.body
+        let matchScoreArr = []
+
+        dataArr.forEach((person) => {
+            let matchScore = 0
+            for (let score in currentPerson.scores) {
+                matchScore += Math.abs(person.scores[score] - currentScores[score]) 
+            }
+            matchScoreArr.push(matchScore)
+            console.log(`Match Score for ${person.name} is ${matchScore}`)
+        })
+        
+        var bestMatch = matchScoreArr.indexOf(Math.min(...matchScoreArr))
+        console.log("Best Match: ")
+        console.log(dataArr[bestMatch])
         dataArr.push(req.body)
     })
 }
