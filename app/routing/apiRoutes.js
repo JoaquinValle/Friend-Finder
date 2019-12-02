6832901155fad1e850244635ac21f9b51218d2e7
Joaquin Valle <jv346@cornell.edu>
2019-12-02 13:15:38 -0600

    fixed express deprecated error by using path dependency and 'path.join'. Server works and paths also work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,11 @@
-// 4. Your `apiRoutes.js` file should contain two routes:
-
-//    * A GET route with the url `/api/friends`. This will be used to display a JSON of all 
-//      possible friends.
-//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. 
-//      This route will also be used to handle the compatibility logic.
-
 const dataArr = require("../data/friends")
 
 module.exports = (app) => {
-    app.get("/api/friends", (res, req) => {
+    app.get("/api/friends", (req, res) => {
         res.json(dataArr)
     })
 
-    app.post("/api/friends", (res, res) => {
+    app.post("/api/friends", (req, res) => {
         dataArr.push(req.body)
     })
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,16 +1,11 @@
-// 3. Your `htmlRoutes.js` file should include two routes:
-
-//    * A GET Route to `/survey` which should display the survey page.
-//    * A default, catch-all route that leads to `home.html` which displays the home page.
-
 const path = require("path")
 
 module.exports = (app) => {
     app.get("/survey", (req, res) => {
-        res.sendfile(__dirname, "../public/survey.html")
+        res.sendfile(path.join(__dirname, "../public/survey.html"))
     })
 
     app.get("*", (req, res) => {
-        res.sendfile(__dirname, "../public/home.html")
+        res.sendfile(path.join(__dirname, "../public/home.html"))
     })
 }
