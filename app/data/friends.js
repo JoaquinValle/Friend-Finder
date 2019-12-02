9537ca043e3980fa8a0bc100718e30f3aa12ff97
Joaquin Valle <jv346@cornell.edu>
2019-12-02 12:31:44 -0600

    added logic for friends.js fixing issues with initial object - has 'dummy' objects for testing

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,34 +1,39 @@
-// 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
+// 5. You should save your application's data inside of `app/data/friends.js` 
+// as an array of objects. Each of these objects should roughly follow the format below.
 
-let object = {
-    name: "name",
+
+// Pasar todos los objetos en dataArr
+let dataArr = [
+  {
+    name: "John",
     photo: "link",
     scores: [4, 3, 2]
-}
+  },
+  {
+    name: "James",
+    photo: "link",
+    scores: [1, 5, 2]
+  }
+]
 
+module.exports= dataArr
+
+// Pasar el objeto de la persona que esta buscando
 let sampleScores = [1, 5, 1]
 
-console.log(object.scores)
-console.log(sampleScores)
-
-let matchScore = 0
-
 // otro for para comparar con todos 
+let matchScoreArr = []
 
-for (let i = 0; i < object.scores.length; i++) {
-    matchScore += Math.abs(object.scores[i] - sampleScores[i]) 
+dataArr.forEach(person => {
+    let matchScore = 0
+    for (let i = 0; i < person.scores.length; i++) {
+        matchScore += Math.abs(person.scores[i] - sampleScores[i]) 
+    }
+    matchScoreArr.push(matchScore)
+    console.log(`Match Score for ${person.name} is ${matchScore}`)
+})
+
+for (let i in matchScoreArr) {
+    dataArr[i].match = matchScoreArr[i]
 }
-
-console.log(matchScore)
-
-// Display pic and prof of best match
-
-// 6. Determine the user's most compatible friend using the following as a guide:
-
-//    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
-//    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
-//      * Example:
-//        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
-//        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
-//        * Total Difference: **2 + 1 + 2 =** **_5_**
-//    * The closest match will be the user with the least amount of difference.
+console.log(dataArr)
