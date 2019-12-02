1e69bc9c790f7087295cbd5c9b23daac96552733
Joaquin Valle <jv346@cornell.edu>
2019-12-02 16:35:50 -0600

    completely fixed logic for finding best match in apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,19 +6,19 @@ module.exports = (app) => {
     })
 
     app.post("/api/friends", (req, res) => {
-        console.log(req.body)
         let currentPerson = req.body
+        console.log(currentPerson)
         let matchScoreArr = []
 
         dataArr.forEach((person) => {
             let matchScore = 0
-            for (let score in currentPerson.scores) {
-                matchScore += Math.abs(person.scores[score] - currentScores[score]) 
+            for (let score in person.scores) {
+                matchScore += Math.abs(parseInt(person.scores[score]) - parseInt(currentPerson.scores[score])) 
             }
             matchScoreArr.push(matchScore)
             console.log(`Match Score for ${person.name} is ${matchScore}`)
         })
-        
+
         var bestMatch = matchScoreArr.indexOf(Math.min(...matchScoreArr))
         console.log("Best Match: ")
         console.log(dataArr[bestMatch])
